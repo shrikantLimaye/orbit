9b12ef403a6da29102f8f9ba7f8d5155deda9d43
Shrikant Limaye <shrikant.limaye@ritikaawood.com>
2022-04-25 11:47:22 +0530

    Complete HTML-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'html_pages/home'
+  root 'html_pages#home'
   get 'html_pages/help'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  root 'application#hello'
+  get 'html_pages/about'
 end
