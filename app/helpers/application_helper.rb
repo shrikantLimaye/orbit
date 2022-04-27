0f5e08f21f7645c262fadc9ed37fdbc56346a446
Shrikant Limaye <shrikant.limaye@ritikaawood.com>
2022-04-27 11:29:31 +0530

    Title Helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+    # Returns a full title on a per page basis
+
+    # Method defination, Optional arguments
+    def full_title(page_title = '')
+
+        # Creating and assigning a variable
+        base_title = "Orbit Feedback Application"
+        
+        # Boolean test
+        if page_title.empty?
+            base_title
+        else
+            page_title + " | " + base_title
+        end
+    end
 end
